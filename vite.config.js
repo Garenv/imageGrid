ada155d8f7e3218d5529e0d766819254842f4fdd
garenv <garenvartanian24@gmail.com>
2023-10-27 22:32:05 -0400

    vite config file has been modified to copy static files into the public directory upon executing npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from "vite";
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import copy from 'rollup-plugin-copy'
 
 export default defineConfig({
     plugins: [
@@ -16,5 +17,11 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
+        copy({
+            targets: [
+                { src: 'resources/assets/vendor/*', dest: 'public/build/assets/vendor' }
+            ],
+            hook: 'writeBundle' // ensure the copy is done after writing the bundle
+        })
     ],
 });
