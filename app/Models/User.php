a8a358780d4bd2a42aa2e67e66ddf47b36ea0a15
garenv <garenvartanian24@gmail.com>
2023-11-15 21:57:39 -0500

    forgot longitude

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,6 +33,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'cityName',
         'zipCode',
         'latitude',
+        'longitude',
         'areaCode',
         'timezone',
         'email_verified_at'
