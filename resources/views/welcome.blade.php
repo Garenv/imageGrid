cda7d893406dc00b7f1c88a99ba43f48808d902a
garenv <garenvartanian24@gmail.com>
2023-11-18 07:08:27 -0500

    added script tag in head in welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,9 @@
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7469058557975267"
+            crossorigin="anonymous"></script>
+
     <!-- Vendor CSS Files -->
     <link href="{{ asset('build/assets/vendor/aos/aos.css') }}" rel="stylesheet">
     <link href="{{ asset('build/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
