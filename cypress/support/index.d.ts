3fb01ba0b11ab138e5ce8f29ca6bd94e9e83ca20
Philosopho <alex.lapeiretta@phopixel.com>
2024-01-13 19:07:03 -0500

    Add description

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -2,7 +2,7 @@
 
 declare namespace Cypress {
     interface Chainable<Subject> {
-        /**
+        /** Clears text of an element and types the text if is non-empty.
          *
          * @param selector
          * @param text
