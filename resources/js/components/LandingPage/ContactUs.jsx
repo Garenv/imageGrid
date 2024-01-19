384ebeb69452c0bb37e668f73ebd02ae6cbd819b
Philosopho <alex.lapeiretta@phopixel.com>
2024-01-18 20:30:01 -0500

    Cleanup

--- resources/js/components/LandingPage/ContactUs.jsx
+++ resources/js/components/LandingPage/ContactUs.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from "react-dom/client";
 import AxiosClient from "../utlities/AxiosClient.jsx";
 import { toast, ToastContainer } from "react-toastify";
