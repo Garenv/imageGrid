0a5c544534063ad3ff38fdd7bbfd2826c9557d4e
garenv <garenvartanian24@gmail.com>
2024-06-05 11:28:05 -0400

    modified canonical tags

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,9 @@
     <meta property="og:type" content="website">
     <meta name="google-adsense-account" content="ca-pub-7469058557975267">
     <link rel="icon" href="https://cruskip.s3.us-east-2.amazonaws.com/assets/images/phopix/logos/p_1081x1080_transparent_v2.1.jpg">
-    <link rel="canonical" href="https://www.phopixel.com/">
+    <link rel="canonical" href="{{ url()->current() }}">
 
+    @dump(url()->current())
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
